463b527f1fe06e899019e87cca5c9f69e95b003a
Alhassan Sahad <98456443+asahad@users.noreply.github.com>
2023-11-23 21:42:32 -0600

    Refactored to include controllers

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express";
 import products from "./data/products.js";
-import productRoutes from "./routes/productRoutes.js"
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
 connectDB();
-const app=express();
-app.use('/api/products',productRoutes)
-app.use('/api/products/:id',productRoutes)
+const app = express();
+app.use("/api/products", productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
