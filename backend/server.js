402eddd319b0ff982e5de1b14b36f6ea9748e2f2
Alhassan Sahad <98456443+asahad@users.noreply.github.com>
2023-12-28 13:00:02 -0600

    deploy

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,7 @@ const app = express();
 
 // CORS configuration
 if (process.env.NODE_ENV === "development") {
-  // Allow requests from your frontend dev server address
-  app.use(cors({ origin: "http://localhost:5173" }));
+  app.use(cors({ origin: "http://localhost:5173" })); // Replace with your frontend dev server address
 }
 
 // Body Parser
@@ -31,42 +30,32 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie Parser
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 // API routes
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
+// PayPal API route
 app.get("/api/config/paypal", (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID);
+  res.send(process.env.PAYPAL_CLIENT_ID || "YOUR-PAYPAL-CLIENT-ID");
 });
 
 // Static files serving for production
 const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from frontend's dist directory
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-
-
-app.use(
-  "/fronted/public/images",
-  express.static(path.join(__dirname, "/fronted/public/images"))
-);
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  // Serve index.html for all unmatched GET requests (for SPA routing)
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
 }
+
 // Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
 
+// Start server
 app.listen(port, () => console.log(`Server running on port ${port}`));
